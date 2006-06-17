7b1b433a12b64b27d2eaa660d6c0d099ad1116eb
Petr Machata <pmachata@gmail.com>
2006-06-17 16:34:27 +0000

    Adding growable string module.  Taken from formal languages class project and modified a bit.  With selftest.
    git-svn-id: svn://kenshin.include.cz/gcc-algol/trunk@4 b2c85cd8-e0fb-da11-a16e-00096ba5af3e

---
+++ al60l/estring.c
@@ -0,0 +1,449 @@
+/*
+ * Copyright (c) 2003 Petr Machata, Jiøí Moskovèák, Kry¹tof Oczadlý
+ * Copyright (c) 2006 Petr Machata
+ * All rights reserved.
+ */
+
+#ifndef SELF_TEST
+
+#include <stdlib.h>
+#include <assert.h>
+
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+
+#include "estring.h"
+
+typedef struct struct_estring_rep_t
+{
+  /// actual length of string - number of used chars, not including
+  /// terminating \0
+  size_t length;
+
+  /// how much the string can grow before it needs a
+  /// realloc. length<limit always holds
+  size_t limit;
+
+  /// underlying character string - actual body of the string
+  char*  body;
+} estring_rep_t;
+
+
+/// Grow string length to 'length' chars. Returns 0 if all goes well,
+/// or -1 if something fails.
+static int
+private_estr_realloc (estring_rep_t * dest, size_t limit)
+{
+  assert(limit > 0);
+
+  if (dest->limit >= limit)
+    return 0;
+
+  // align limit
+  size_t real_limit = limit;//(1 + limit/64)*64;
+
+  char * new_body = (char*)realloc (dest->body, real_limit);
+  if (new_body == NULL)
+    return -1;
+
+  dest->body = new_body;
+  char *old_end = dest->body + dest->limit;
+  dest->limit = real_limit;
+
+  // clean tail
+  memset (old_end, 0, real_limit - (old_end - dest->body)); ///@TODO:check!
+
+  return 0;
+}
+
+/// Grow string length by 128 chars. Returns 0 if all goes well, or -1
+/// if something fails.
+static int
+private_estr_grow (estring_rep_t * dest)
+{
+  return private_estr_realloc (dest, dest->limit + 128);
+}
+
+void
+estr_stats (estring_t * _dest)
+{
+  estring_rep_t * dest = (void*)_dest;
+  printf("estr_stats(%p)\n", (void*)dest);
+  printf("  body:\t%p\n",   dest->body);
+  printf("  length:\t%i\n", dest->length);
+  printf("  limit:\t%i\n",  dest->limit);
+}
+
+estring_t *
+estr_new (void)
+{
+  estring_rep_t * ret = malloc (sizeof (estring_rep_t));
+  if (ret == NULL)
+    return NULL;
+
+  ret->body = NULL;
+  ret->length = 0;
+  ret->limit = 0;
+
+  if (private_estr_realloc (ret, 1))
+    return NULL;
+  *ret->body  = '\0';
+
+  return (void*)ret;
+}
+
+estring_t *
+estr_new_from (char const* src)
+{
+  assert (src != NULL);
+
+  estring_t * ret = estr_new ();
+  estr_append_cstr(ret, src);
+  return ret;
+}
+
+estring_t *
+estr_clone (estring_t * src)
+{
+  assert (src != NULL);
+
+  estring_t * ret = estr_new ();
+  if (ret == NULL)
+    return NULL;
+
+  estr_append (ret, src);
+
+  return ret;
+}
+
+int
+estr_assign_cstr (estring_t * _dest, char const* src)
+{
+  assert (src != NULL);
+  assert (_dest != NULL);
+
+  estring_rep_t * dest = (void*)_dest;
+  size_t src_len = strlen (src);
+
+  if (private_estr_realloc (dest, src_len + 1))
+    return -1;
+
+  strcpy (dest->body, src);
+  dest->length = src_len;
+
+  return 0;
+}
+
+int
+estr_assign (estring_t * dest, estring_t * _src)
+{
+  assert (_src != NULL);
+  assert (dest != NULL);
+
+  estring_rep_t * src = (void*)_src;
+  return estr_assign_cstr (dest, src->body);
+}
+
+void
+estr_delete (estring_t * _dest)
+{
+  assert (_dest != NULL);
+  estring_rep_t * dest = (void*)_dest;
+  assert (dest->body != NULL);
+
+  free (dest->body);
+  free (dest);
+}
+
+void
+estr_clear (estring_t * _dest)
+{
+  assert (_dest != NULL);
+  estring_rep_t * dest = (void*)_dest;
+
+  memset (dest->body, 0, dest->length);
+  dest->length = 0;
+}
+
+char const*
+estr_cstr (estring_t * _dest)
+{
+  assert (_dest != NULL);
+  estring_rep_t * dest = (void*)_dest;
+  return dest->body;
+}
+
+void
+estr_tolcase (estring_t * _dest)
+{
+  assert (_dest != 0);
+  estring_rep_t * dest = (void*)_dest;
+
+  char * body = dest->body;
+  char * bend = body + dest->length -1;
+
+  while (body <= bend)
+    {
+      *body = tolower (*body);
+      ++body;
+    }
+}
+
+long
+estr_tonumber (estring_t * _dest)
+{
+  assert (_dest != 0);
+  estring_rep_t * dest = (void*)_dest;
+
+  return atol (dest->body);
+}
+
+double
+estr_tofloat (estring_t * _dest)
+{
+  assert (_dest != 0);
+  estring_rep_t * dest = (void*)_dest;
+  return atof (dest->body);
+}
+
+/// Append given count of characters from source char* string to
+/// destination estring. Used internally by estr_append functions.
+static int
+private_estr_nappend (estring_rep_t * dest, char const* src, size_t src_length)
+{
+  assert (dest != NULL);
+  size_t future_length = dest->length + src_length;
+
+  // if src==dest, we will have to actualize src after the
+  // realoocation of dest
+  int reloc = dest->body == src;
+  if (private_estr_realloc (dest, future_length + 1))
+    return -1;
+  if (reloc)
+    src = dest->body;
+
+  char * body_end = dest->body + dest->length;
+  strncpy (body_end, src, src_length);
+
+  dest->length = future_length;
+
+  return 0;
+}
+
+int
+estr_append_cstr (estring_t * _dest, char const* src)
+{
+  assert (src != NULL);
+  assert (_dest != NULL);
+  estring_rep_t * dest = (void*)_dest;
+
+  return private_estr_nappend (dest, src, strlen (src));
+}
+
+int
+estr_append (estring_t * _dest, estring_t * _src)
+{
+  assert (_src != NULL);
+  assert (_dest != NULL);
+  estring_rep_t * dest = (void*)_dest;
+  estring_rep_t * src = (void*)_src;
+
+  return private_estr_nappend (dest, src->body, src->length);
+}
+
+/// Prepend given count of characters from source char* string to
+/// destination estring. Used internally by estr_prepend functions.
+int
+private_estr_nprepend (estring_rep_t * dest, char const* src, size_t src_length)
+{
+  size_t future_length = dest->length + src_length;
+
+  // if src==dest, we will have to actualize src after the
+  // realoocation of dest
+  int reloc = src == dest->body;
+  if (private_estr_realloc (dest, future_length + 1))
+    return -1;
+  if (reloc)
+    src = dest->body;
+
+  char * fut_body_begin = dest->body + src_length;
+  memmove (fut_body_begin, dest->body, dest->length);
+  strncpy (dest->body, src, src_length);
+  dest->length = future_length;
+
+  return 0;
+}
+
+int
+estr_prepend_cstr (estring_t * _dest, char const* src)
+{
+  assert (src != NULL);
+  assert (_dest != NULL);
+  estring_rep_t * dest = (void*)_dest;
+
+  return private_estr_nprepend (dest, src, strlen (src));
+}
+
+int
+estr_prepend (estring_t * _dest, estring_t * _src)
+{
+  assert (_src != NULL);
+  assert (_dest != NULL);
+  estring_rep_t * dest = (void*)_dest;
+  estring_rep_t * src = (void*)_src;
+
+  return private_estr_nprepend (dest, src->body, src->length);
+}
+
+int
+estr_push (estring_t * _dest, char what)
+{
+  assert (_dest != NULL);
+  estring_rep_t * dest = (void*)_dest;
+
+  if (dest->length+1 >= dest->limit)
+    if (private_estr_grow(dest))
+      return -1;
+
+  char * body_end = dest->body + dest->length;
+  *body_end = what;
+  // unused chars are always zero, so no extra care is to be taken
+
+  dest->length++;
+
+  return 0;
+}
+
+int
+estr_pop (estring_t * _dest)
+{
+  assert (_dest != NULL);
+  estring_rep_t * dest = (void*)_dest;
+
+  if (dest->length > 0)
+    {
+      --dest->length;
+      char * body_end = dest->body + dest->length;
+      char ret = *body_end;
+      *body_end = '\0';
+
+      return ret;
+    }
+  else
+    return EOF;
+}
+
+int
+estr_compare (estring_t * _src1, estring_t * _src2)
+{
+  assert (_src2 != NULL);
+  estring_rep_t * src2 = (void*)_src2;
+
+  return estr_compare_cstr (_src1, src2->body);
+}
+
+int
+estr_compare_cstr (estring_t * _src1, char const * src2)
+{
+  assert (_src1 != NULL);
+  assert (src2 != NULL);
+  estring_rep_t * src1 = (void*)_src1;
+
+  return strcmp (src1->body, src2);
+}
+
+size_t
+estr_length (estring_t * _dest)
+{
+  assert (_dest != NULL);
+  estring_rep_t * dest = (void*)_dest;
+  return dest->length;
+}
+
+char
+estr_at (estring_t * _dest, int position)
+{
+  assert (_dest != NULL);
+  estring_rep_t * dest = (void*)_dest;
+
+  if (position < 0
+      || position >= dest->length)
+    return EOF;
+  else
+    return dest->body[position];
+}
+
+void
+estr_write (estring_t * _dest, char c, int position)
+{
+  assert (_dest != NULL);
+  estring_rep_t * dest = (void*)_dest;
+
+  dest->body[position] = c;
+}
+
+#else /* SELF_TEST */
+
+#include "estring.h"
+#include <assert.h>
+#include <string.h>
+#include <stdio.h>
+
+int
+main (void)
+{
+  printf ("new/clone test.\n");
+  estring_t * str1 = estr_new_from ("ahoj svete");
+  assert (estr_compare (str1, str1) == 0);
+
+  estring_t * str2 = estr_new_from ("ahoj svete");
+  assert (estr_length (str1) == strlen ("ahoj svete"));
+  assert (estr_compare (str1, str2) == 0);
+
+  estring_t * str3 = estr_clone (str1);
+  estr_delete (str1);
+  assert (estr_compare (str2, str3) == 0);
+  estr_append (str2, str3);
+  estr_append (str3, str3);
+  assert (estr_compare (str2, str3) == 0);
+
+  printf ("push/pop test.\n");
+  estring_t * str4 = estr_new ();
+  int c;
+  while ((c = estr_pop (str3)) != EOF)
+    estr_push (str4, c);
+  assert (estr_compare_cstr (str4, "etevs johaetevs joha") == 0);
+
+  estr_delete (str2);
+  estr_delete (str3);
+  estr_delete (str4);
+
+  printf ("append/prepend:\n");
+  int (*functions[])(estring_t *, estring_t *) = {estr_append, estr_prepend, NULL};
+
+  for (int (**it)(estring_t *, estring_t *) = &functions[0];
+       *it != NULL;
+       it++)
+    {
+      printf (" + test...\n");
+      estring_t * str5 = estr_new_from ("ab");
+      assert (estr_length (str5) == 2);
+      for (int i = 0; i < 20; ++i)
+	{
+	  size_t s = estr_length (str5);
+	  (*it) (str5, str5);
+	  assert (estr_length (str5) == s * 2);
+	}
+      for (int i = 0; i < estr_length (str5); ++i)
+	assert (estr_at (str5, i) == i%2?'a':'b');
+      assert (estr_at (str5, estr_length (str5)) == EOF);
+      assert (estr_at (str5, -54) == EOF);
+      estr_delete (str5);
+    }
+
+  printf ("All passed.\n");
+  return 0;
+}
+
+#endif
---
+++ al60l/estring.h
@@ -0,0 +1,132 @@
+/*
+ * Copyright (c) 2003 Petr Machata, Jiøí Moskovèák, Kry¹tof Oczadlý
+ * Copyright (c) 2006 Petr Machata
+ * All rights reserved.
+ */
+#ifndef _AL60L_ESTRING_H_
+#define _AL60L_ESTRING_H_
+
+#include <stdlib.h>
+#include "pd.h"
+
+///
+///
+/// \file estring.h
+///
+/// `endless' strings library.  Estrings are basically the same
+/// strings that you use when working with char*. Major difference is,
+/// that you don't have to know how long the string will eventually be
+/// - estrings are `smart', and grow as necessary as you add new
+/// characters to them.
+///
+
+
+typedef struct struct_estring_t { } estring_t;
+
+
+/// Debugging info for estring.
+void estr_stats (estring_t * dest)
+     ARG_NONNULL(1);
+
+/// Create empty new string. Please note, that such a string has to be
+/// released with estr_delete(). Returns NULL if error is something ill
+/// happens.
+estring_t * estr_new (void)
+     MALLOC_LIKE;
+
+/// Create new string by copying contents of 'src'. 'src' has to be
+/// null-terminated string.
+estring_t * estr_new_from (char const* src)
+     ARG_NONNULL(1)
+     MALLOC_LIKE;
+
+/// Create a string as 1:1 copy of another string. Returns NULL if
+/// something fails.
+estring_t * estr_clone (estring_t * src)
+     ARG_NONNULL(1)
+     MALLOC_LIKE;
+
+/// Release memory allocated for string.
+void estr_delete (estring_t * str);
+
+/// Assign a new value to given string. Returns 0 on success or -1 if
+/// something failed. In such a case, original string is left intact.
+/// This clones body of string, thus 'src' is usable after this call.
+int estr_assign_cstr (estring_t * dest, char const* src)
+     ARG_NONNULL(1)
+     ARG_NONNULL(2);
+
+/// Like estr_assign_cstr, but for estring.
+int estr_assign (estring_t * dest, estring_t * src)
+     ARG_NONNULL(1)
+     ARG_NONNULL(2);
+
+/// Clear a string - won't release memory, just truncates the string
+/// body.
+void estr_clear (estring_t * dest)
+     ARG_NONNULL(1);
+
+/// Convert endless string to classic c-style char* string (for example
+/// to make it possible to printf() it).
+///
+/// Warning: pointer is guaranteed to be valid *only* until you call
+/// another Str_* functions. Some string handling functions may require
+/// the string to grow, and reallocating memory needed for string data
+/// may invalidate value returned by this function.
+char const* estr_cstr (estring_t * str)
+     ARG_NONNULL(1);
+
+/// Convert estring to lower case.
+void estr_tolcase (estring_t * str)
+     ARG_NONNULL(1);
+
+/// Give back numerical value of string, with base 10.
+long estr_tonumber (estring_t * str)
+     ARG_NONNULL(1);
+
+/// Like tonumber, but returns a double.
+double estr_tofloat(estring_t * str);
+
+/// Append contents of 'src' to the end of 'dest'.  Return 0 if all
+/// goes fine, or -1 if something went wrong.
+int estr_append_cstr(estring_t * dest, char const* src);
+
+/// Like estr_append_cstr, but append estring instead of raw c string.
+int estr_append (estring_t * dest, estring_t * src);
+
+/// Similar to estr_append_cstr, but contents of 'src' is prepended
+/// before contents of 'dest'.  Returns 0 if OK, otherwise -1.
+int estr_prepend_cstr (estring_t * dest, char const* src);
+
+/// Like estr_prepend_cstr, but prepend estring instead of raw c
+/// string.
+int estr_prepend (estring_t * dest, estring_t * src);
+
+/// Append a single character to the end of estring.  Returns 0 if all
+/// goes well, or -1 if something fails (such as we cannot grow more).
+int estr_push (estring_t * dest, char what);
+
+/// Pop a single character off the end of estring.  Returns EOF if the
+/// string has no more poppable chars.
+int estr_pop (estring_t * dest);
+
+/// Compare two estrings.  It returns an integer less than, equal to, or
+/// greater than zero if src1 is found, respectively, to be less than,
+/// to match, or be greater than src2.
+int estr_compare (estring_t * src1, estring_t * src2);
+
+/// Compare estring with cstr.
+int estr_compare_cstr (estring_t * src1, char const * src2);
+
+/// Return length of string 'src'.  Note that this is a constant-time
+/// operation, as estrings remember their length.
+size_t estr_length (estring_t * src);
+
+/// Give back char at 'position'-th position of string 'src'.  Return
+/// EOF for characters off the boundaries.
+char estr_at (estring_t * src, int position);
+
+/// Write a char to given location.
+void estr_write (estring_t * src, char c, int position);
+
+#endif//_AL60L_ESTRING_H_
